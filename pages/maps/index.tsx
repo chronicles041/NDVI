b964deed5ababbc6a9a0d00790b3dbf5e1725e28
Dibash182 <dibashpoudel182@gmail.com>
2022-03-06 16:52:59 +0545

    New  Changes

--- pages/maps/index.tsx
+++ pages/maps/index.tsx
@@ -1,29 +1,24 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MapBody from "./mapBody";
 
 const Maps = () => {
     const position = [51.505, -0.09]
-    if (typeof window !== "undefined") {
-       alert('No Window')
-      }
+    // if (typeof window !== "undefined") {
+    //    alert('No Window')
+    //   }
+
+
+
+  
   return (
 
     <div className="flex flex-row">
       <div className=" bg-white basis-8/12">
-        <MapContainer center={[51.505, -0.09]} zoom={13}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+          {/* <MapBody /> */}
       </div>
       <div className="bg-white basis-4/12">Other Info</div>
     </div>
   );
+    
 };
 
 export default Maps;
