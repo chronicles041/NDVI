84d7c448183882eec8e212ee4c78bb0e52ae788b
Dibash182 <dibashpoudel182@gmail.com>
2022-04-17 11:16:39 +0545

    Recent Changes

--- components/ToTable/pagination.tsx
+++ components/ToTable/pagination.tsx
@@ -1,19 +1,26 @@
 type PaginationProps = {
-    loading : boolean;
-    page : number;
-    pageCount : number;
-    pageSize : number
-    canPreviousPage:boolean;
-    canNextPage:boolean;
-    pageIndex:number;
-    pageOptions:number[];
-  };
-  
+  loading: boolean;
+  page: number;
+  pageCount: number;
+  pageSize: number;
+  canPreviousPage: boolean;
+  canNextPage: boolean;
+  pageIndex: number;
+  pageOptions: number[];
+  setPageSize: Function;
+};
 
-
-export const ToTablePagination = ({loading,page,pageCount,pageIndex,pageOptions,pageSize,canNextPage,canPreviousPage}:PaginationProps) => {
-
-
+export const ToTablePagination = ({
+  loading,
+  page,
+  pageCount,
+  pageIndex,
+  pageOptions,
+  pageSize,
+  canNextPage,
+  canPreviousPage,
+  setPageSize
+}: PaginationProps) => {
   return (
     <div className="pagination flex item-center justify-between w-full my-4 mx-4 ,mt-1">
       <div className="mx-6 item-center">
@@ -22,7 +29,7 @@ export const ToTablePagination = ({loading,page,pageCount,pageIndex,pageOptions,
           <div>Loading...</div>
         ) : (
           <div className="text-lg font-semibold bg-primary px-2 py-2 text-center text-white shadow-2xl rounded-2xl opacity-80 ">
-            Showing {page} of ~{pageCount * pageSize} results
+            Showing {pageSize} of ~{pageCount * pageSize} results
           </div>
         )}
       </div>
@@ -74,7 +81,7 @@ export const ToTablePagination = ({loading,page,pageCount,pageIndex,pageOptions,
         <span className="text-lg font-medium">
           Page{" "}
           <strong>
-            {pageIndex + 1} of {pageOptions.length}
+            {page} of {pageCount}
           </strong>{" "}
         </span>
         <span className="text-lg font-medium">
@@ -82,21 +89,24 @@ export const ToTablePagination = ({loading,page,pageCount,pageIndex,pageOptions,
           <input
             type="number"
             className="rounded-lg focus:ring-primary"
-            defaultValue={pageIndex + 1}
-            // onChange={(e) => {
-            //   const page = e.target.value ? Number(e.target.value) - 1 : 0;
-            //   gotoPage(page);
-            // }}
+            defaultValue={page}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              // alert(page)
+              // gotoPage(page);
+            }}
             style={{ width: "100px" }}
           />
         </span>{" "}
         <select
           value={pageSize}
-          disabled={true}
-          hidden={true}
-          //   onChange={(e) => {
-          //     setPageSize(Number(e.target.value));
-          //   }}
+          // disabled={true}
+          // hidden={true}
+          // onChange={(e) => {
+          //   setPageSize(Number(e.target.value));
+          // }}
+          // onChange={(e)=>alert(e.target.value)}
+          onChange={(e)=>setPageSize(e.target.value)}
         >
           {[5, 10, 20, 30, 40, 50].map((pageSize) => (
             <option key={pageSize} value={pageSize}>
