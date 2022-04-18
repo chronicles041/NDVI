12ffeaf55a5f874091bff8019a3f2d9f1568cbf6
opensworup <sworup31@gmail.com>
2022-04-18 19:27:30 +0545

    Field Detail done

--- pages/reports/fieldDetail.tsx
+++ pages/reports/fieldDetail.tsx
@@ -37,7 +37,7 @@ const DetailModal = ({ id }: { id: number }) => {
 
   return (
     <ToModal title={"Field Details"}>
-      <div className="grid w-full grid-cols-2  gap-4 p-5 h-full">
+      <div className="grid w-full grid-cols-2  gap-2 p-5  h-full">
         <div className="bg-white border-2  overflow-hidden border-solid border-primary flex flex-col justify-center items-center   p-10 rounded-lg">
           <ToIcon
             type={IconTypes.Farm}
@@ -75,21 +75,48 @@ const DetailModal = ({ id }: { id: number }) => {
             style={IconStyles.Default}
           ></ToIcon>
           <div className="text-center font-normal text-secondary whitespace-nowrap  text-base mt-8 p-4">
-            Organization Name: 
+            Organization Name:
             <br></br>
             {detail.organization_name}
           </div>
         </div>
         <div className="bg-white flex flex-col justify-center items-center border-2 border-solid border-primary   p-10 rounded-lg">
-        <ToIcon
+          <ToIcon
             type={IconTypes.Land}
             size={IconSize.MD}
             style={IconStyles.Default}
           ></ToIcon>
-        <div className="text-center font-normal text-secondary text-base mt-8 p-4 ">
-          Farm Area: {detail.farm_area}
+          <div className="text-center font-normal text-secondary text-base mt-8 p-4 ">
+            Farm Area: {detail.farm_area}
+          </div>
         </div>
+        <div className="bg-white flex flex-col justify-center items-center border-2 border-solid border-primary   p-10 rounded-lg">
+          <ToIcon
+            type={IconTypes.Crop}
+            size={IconSize.MD}
+            style={IconStyles.Default}
+          ></ToIcon>
+          <div className="text-center font-normal text-secondary text-base mt-8 p-4 ">
+            Farm Note: {detail.farm_notes}
+          </div>
         </div>
+        {detail.farmer_info.map((data) => (
+          <div className="flex flex-col">
+            <div>Name: {data.farmer_name}</div>
+            <div>Phone: {data.farmer_phone}</div>
+            <div>Gender: {data.additional_fields.gender}</div>
+            <div>Ownership Type: {data.additional_fields.self_owned}</div>
+
+
+          </div>
+        ))}
+        {/* {Object.entries(detail.farmer_info).map(([key, value ]) => {
+          return (
+            <div >
+              {key} : {value}
+            </div>
+          );
+        })} */}
       </div>
     </ToModal>
   );
