b6c3db63da81b857621e32eb6e1062397d4ab5e4
Dibash182 <dibashpoudel182@gmail.com>
2022-04-20 11:11:55 +0545

    API Intigreted

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -10,17 +10,16 @@ const Login = () => {
   const [message, changeMessage] = useState("");
   // alert("Reached")
   const createToken = () => {
-    Router.push("/dashboard");
+    // Router.push("/dashboard");
 
     axios
-      .post("https://app.teamonetech.com/auth/token/login/", {
+      .post("https://app.teamonetech.com/custom-auth/token/login/", {
         username: username,
         password: password,
       })
       .then((response) => {
-        console.log(response);
-        localStorage.setItem('token','token')
-       localStorage.setItem('token','token')
+        console.log("Response",response.data.token);
+        localStorage.setItem('token',response.data.token)
         Router.push("/dashboard");
       })
       .catch((err) => {
