02e2b502f16f4022d57558022031e3bd27501859
Dibash182 <dibashpoudel182@gmail.com>
2022-04-20 14:05:35 +0545

    Devlopment Merged

--- pages/maps/index.tsx
+++ pages/maps/index.tsx
@@ -4,10 +4,10 @@ import PageLayout from "../../components/Pagelayout";
 // import Map from './Map'
 
 // const ToMap = dynamic(() => import("./Map"), { ssr: false });
+const Map = dynamic(() => import("../../components/ToMap/Map.js"), { ssr: false });
 
 const MapIndex = () => {
   // const Map = React.useMemo(() => dynamic(() => import('./Map'), { loading: () => <p>Loading map...</p>, ssr: false, }), [])
-  const Map = dynamic(() => import("../../components/ToMap/Map.js"), { ssr: false });
   return (
     <PageLayout>
       {/* <ToMap /> */}
