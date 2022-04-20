25b0b4a21f63fa1a476c81cf90364d0316c93b6b
Dibash182 <dibashpoudel182@gmail.com>
2022-04-20 13:55:59 +0545

    Dashboard

--- components/Stats/index.tsx
+++ components/Stats/index.tsx
@@ -1,50 +1,33 @@
 import ToIcon, { IconSize, IconStyles, IconTypes } from "../ToIcons";
-const Routes: any[] = [
-  {
-    title: "Total Champions",
-    icon: IconTypes.Champions,
-    data: 3
-  },
-  {
-    title: "Total Users ",
-    icon: IconTypes.User,
-    data: 20
-  },
-  {
-    title: "Total Fields Visited",
-    icon: IconTypes.Land,
-    data: "50+"
-  },
-  {
-    title: "Total District",
-    icon: IconTypes.Farm,
-    data: "3"
-  },
-  {
-    title: "Total Communites",
-    icon: IconTypes.Community,
-    data: "-"
-  },
-  {
-    title: "Total Crop",
-    icon: IconTypes.Crop,
-    data: 1
-  },
-  {
-    title: "Average NDVI",
-    icon: IconTypes.Average,
-    data: "-"
-  },
-  {
-    title: "Top Dressing",
-    icon: IconTypes.Dressing,
-    data: 20
-  },
-];
+import Router from "next/router";
 
-export const Stats = () => (
-  <div className="grid grid-cols-4 mb-10">
-    {Routes.map((route) => (
+type StatProps = {
+  title: string;
+  icon: typeof IconTypes;
+  count: number;
+  path:string;
+  buttonText:string;
+};
+
+export const Stats = ({ title, icon, count,path,buttonText }: StatProps) => (
+  <div className="mb-10">  
+    <div className="bg-white border-2 border-primary p-6 m-12 rounded-lg shadow-lg flex flex-col justify-center ">
+      <ToIcon type={icon} size={IconSize.MD} style={IconStyles.Default} />
+      <div className="text-center font-semibold text-secondary text-3xl mt-8 p-4">
+        {count} +
+      </div>
+      <div className="text-center font-semibold text-secondary text-lg mt-2 px-2">
+        {title}
+      </div>
+      <button 
+      onClick={()=>Router.push(path)}
+      >{buttonText}</button>
+    </div>
+  </div>
+);
+
+
+{/* {Routes.map((route) => (
       <div className="bg-white border-2 border-primary p-6 m-12 rounded-lg shadow-lg flex flex-col justify-center ">
         <ToIcon
           type={route?.icon}
@@ -58,6 +41,4 @@ export const Stats = () => (
           {route.title}
         </div>
       </div>
-    ))}
-  </div>
-);
+    ))} */}
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,11 +1,37 @@
-import { DashBoradComponent } from "../../components/Dashboard";
 import PageLayout from "../../components/Pagelayout";
+import { Stats } from "../../components/Stats";
+import { IconTypes } from "../../components/ToIcons";
 import Reports from "../reports";
 
 const DashBorad = () => {
   return (
     <PageLayout>
-      <DashBoradComponent />
+      <div className={" p-6  m-3 "}>
+        <h2 className="text-xl font-bold mb-2 text-gray-800">{"DashBorad"}</h2>
+        <div className="grid  grid-cols-3 mb-5">
+          <Stats
+            icon={IconTypes.Dressing}
+            title={"Total Farms"}
+            count={260}
+            buttonText={"View Reports"}
+            path={"/reports"}
+          />
+          <Stats
+            icon={IconTypes.Dressing}
+            title={"Total Farm Digitalized"}
+            count={20}
+            buttonText={"View Maps"}
+            path={"/maps"}
+          />
+          <Stats
+            icon={IconTypes.Dressing}
+            title={"Total Farmers"}
+            count={228}
+            buttonText={"View Reports"}
+            path={"/reports"}
+          />
+        </div>
+      </div>
     </PageLayout>
   );
 };
